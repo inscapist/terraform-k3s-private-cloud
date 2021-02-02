fb13d5a493242f7b2e9f79b150a94561a5f67544
Felix <zen9.felix@gmail.com>
2021-02-02 19:20:19 +0800

    Add default node ports discovery

--- security.tf
+++ security.tf
@@ -17,6 +17,19 @@ resource "aws_security_group" "self" {
   })
 }
 
+resource "aws_security_group" "node-ports" {
+  name        = "${local.cluster_id}-node-ports"
+  vpc_id      = data.aws_vpc.this.id
+  description = "Allow node ports to be discovered"
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "egress" {
   name        = "${local.cluster_id}-egress"
   vpc_id      = data.aws_vpc.this.id
